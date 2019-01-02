a10fa6cd8eb16075550466074e77ff0e601bc752
Melroy Pereira <46313772+CodeHydra@users.noreply.github.com>
2019-01-02 14:45:11 +0530

    Add files via upload

---
+++ degree to radian.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  2 06:38:15 2019
+
+@author: Melroy Pereira
+"""
+
+#######    DEGREE TO RADIAN
+
+
+
+import math
+Degree= float(input("enter the value of degree"))
+radian=(Degree*math.pi)/180
+print("the radian is: ",float(radian))
